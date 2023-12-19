6ed7e2f8a89139637f8d3ea05075f8fda53a9853
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2023-12-19 08:26:24 +0000

    Used lat and lng from Google Maps API to make GET request to weather API and display current weather summary and temperature

--- umbrella.rb
+++ umbrella.rb
@@ -8,18 +8,26 @@ input_location = input_location.gsub(" ", "%20")
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 gmaps_api_key = ENV.fetch("GMAPS_KEY")
 
-pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/41.8887,-87.6355"
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{input_location}&key=#{gmaps_api_key}"
 
-raw_resp = HTTP.get(maps_url)
-parsed_response = JSON.parse(raw_resp)
+raw_maps_resp = HTTP.get(maps_url)
+parsed_maps_response = JSON.parse(raw_maps_resp)
 
-results = parsed_response["results"]
-first_result = results[0]
-geo = first_result["geometry"]
+maps_results = parsed_maps_response["results"]
+maps_first_result = maps_results[0]
+geo = maps_first_result["geometry"]
 location = geo["location"]
 latitude = location["lat"]
 longitude = location["lng"]
 
-pp location
-# pp raw_resp.to_s
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/#{latitude},#{longitude}"
+
+raw_weather_resp = HTTP.get(pirate_weather_url)
+parsed_weather_resp = JSON.parse(raw_weather_resp)
+current_weather = parsed_weather_resp["currently"]
+current_weather_summary = current_weather["summary"]
+current_weather_temperature = current_weather["temperature"]
+
+puts "The current weather is #{current_weather_summary.downcase}, with a temperature of #{current_weather_temperature}"
+# pp current_weather_summary
+# pp current_weather_temperature

f79e7203e111febbf34351aa91302211c82af38d
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2023-12-19 08:42:55 +0000

    Small change to make JSON.parse intake HTTP::Response that's been converted to string for better practice

--- umbrella.rb
+++ umbrella.rb
@@ -11,7 +11,7 @@ gmaps_api_key = ENV.fetch("GMAPS_KEY")
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{input_location}&key=#{gmaps_api_key}"
 
 raw_maps_resp = HTTP.get(maps_url)
-parsed_maps_response = JSON.parse(raw_maps_resp)
+parsed_maps_response = JSON.parse(raw_maps_resp.to_s)
 
 maps_results = parsed_maps_response["results"]
 maps_first_result = maps_results[0]
@@ -23,11 +23,12 @@ longitude = location["lng"]
 pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/#{latitude},#{longitude}"
 
 raw_weather_resp = HTTP.get(pirate_weather_url)
-parsed_weather_resp = JSON.parse(raw_weather_resp)
+parsed_weather_resp = JSON.parse(raw_weather_resp.to_s)
 current_weather = parsed_weather_resp["currently"]
 current_weather_summary = current_weather["summary"]
 current_weather_temperature = current_weather["temperature"]
 
 puts "The current weather is #{current_weather_summary.downcase}, with a temperature of #{current_weather_temperature}"
+
 # pp current_weather_summary
 # pp current_weather_temperature
